50825fac151abb93e8cf7f225343c0fb46e7dbcb
KONAKESHAVAREDDY <104962709+KONAKESHAVAREDDY@users.noreply.github.com>
2023-07-06 18:07:10 +0530

    Create Create-Ticket.py

---
+++ actions/Create-Ticket.py
@@ -0,0 +1,11 @@
+from lib.actions import BaseAction
+
+
+class CreateIncidentAction(BaseAction):
+    def run(self,payload):
+        s = self.client
+
+        path = '/table/incident'
+        response = s.resource(api_path=path).create(payload=payload)
+        sys_id= response.get('sys_id')
+        return sys_id
