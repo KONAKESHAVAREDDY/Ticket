984f623ea23890ab5ab138c7cd5346655d80c4be
KONAKESHAVAREDDY <104962709+KONAKESHAVAREDDY@users.noreply.github.com>
2023-07-06 18:15:46 +0530

    Create actions.py

---
+++ actions/lib/actions.py
@@ -0,0 +1,18 @@
+from st2common.runners.base_action import Action
+import pysnow as sn
+class BaseAction(Action):
+    def __init__(self, config=None, action_service=None):
+        super(BaseAction, self).__init__(config, action_service)
+        self.client = self._get_client()
+
+    def _get_client(self):
+        instance_name = self.config['instance_name']
+        username = self.config['username']
+        password = self.config['password']
+
+        client = sn.Client(instance=instance_name, user=username, password=password)
+
+        if 'custom_params' in self.config and isinstance(self.config['custom_params'], dict):
+            client.parameters.add_custom(self.config['custom_params'])
+
+        return client
